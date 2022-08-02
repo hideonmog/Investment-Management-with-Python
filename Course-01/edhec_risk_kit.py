fbd94a64066297d695b404db33aa92576bf59e03
Mohshin Ahmed <mohshina1@outlook.com>
2022-08-02 23:55:35 +0100

    Add Semideviation, VaR and CVaR functions

--- Course-01/edhec_risk_kit.py
+++ Course-01/edhec_risk_kit.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import scipy.stats
+import numpy as np
 
 def drawdown(return_series: pd.Series):
     '''
@@ -47,6 +48,28 @@ def get_hfi_returns():
     hfi.index = pd.to_datetime(hfi.index, format = '%Y%m').to_period('M')
     return hfi
 
+def semideviation(r):
+    '''
+    Returns the semideviation (negative semideviation) of r
+    r must be a Series or a DataFrame
+    
+    This is an approximation that is used for data with
+    a mean close to zero (daily returns)
+    '''
+    is_negative = r < 0
+    return r[is_negative].std(ddof = 0)
+
+def semideviation3(r):
+    '''
+    Returns the semideviation (negative semideviation) of r
+    r must be a Series or a DataFrame
+    '''
+    excess = r - r.mean()
+    excess_negative = excess[excess < 0]
+    excess_negative_square = excess_negative**2
+    n_negative = (excess < 0).sum()
+    return (excess_negative_square.sum()/n_negative)**0.5
+
 def skewness(r):
     '''
     Alternative to scipy.stats.skew()
@@ -78,4 +101,60 @@ def is_normal(r, level = 0.01):
     Returns True if the hypothesis of normality is accepted, False otherwise
     '''
     statistic, p_value = scipy.stats.jarque_bera(r)
-    return p_value > level 
+    return p_value > level
+
+def var_historic(r, level = 5):
+    '''
+    Returns the historic Value at Risk at a specified level
+    i.e. returns the number such that 'level' percent of the returns
+    fall below that number, and the (100-level) percent are above
+    '''
+    if isinstance(r, pd.DataFrame):
+        return r.aggregate(var_historic, level = level)
+    elif isinstance(r, pd.Series):
+        return -np.percentile(r, level)
+    else:
+        raise TypeError('Expected r to be Series or DataFrame') 
+
+def var_gaussian(r, level = 5, modified = False):
+    '''
+    Returns the Parametric Gaussian VaR of a Series or DataFrame
+    If 'modified' is True, then the modified VaR is returned,
+    using the Cornish-Fisher modification
+    '''
+    # compute z score assuming gaussian
+    z = scipy.stats.norm.ppf(level/100)
+    if modified:
+        # modify z score based on observed skewness and kurtosis
+        s = skewness(r)
+        k = kurtosis(r)
+        z = (z +
+                (z**2 - 1)*s/6 +
+                (z**3 - 3*z)*(k - 3)/24 +
+                (2*z**3 - 5*z)*(s**2)/36
+            )
+    return -(r.mean() + z*r.std(ddof = 0))
+
+def cvar_historic(r, level = 5):
+    '''
+    Computes the Conditional VaR of Series or DataFrame
+    '''
+    if isinstance(r, pd.Series):
+        is_beyond = r <= -var_historic(r, level = level)
+        return -r[is_beyond].mean()
+    elif isinstance(r, pd.DataFrame):
+        return r.aggregate(cvar_historic, level = level)
+    else:
+        raise TypeError('Expected r to be a Series or DataFrame')
+
+def cvar_gaussian(r, level = 5, modified = False):
+    '''
+    Computes the Conditional VaR of Series or DataFrame
+    '''
+    if isinstance(r, pd.Series):
+        is_beyond = r <= -var_gaussian(r, level = level, modified = modified)
+        return -r[is_beyond].mean()
+    elif isinstance(r, pd.DataFrame):
+        return r.aggregate(cvar_gaussian, level = level, modified = modified)
+    else:
+        raise TypeError('Expected r to be a Series or DataFrame')
