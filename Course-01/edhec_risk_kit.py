8a293e660e051c3e374bd3ec9929b6210444a509
Mohshin Ahmed <mohshina1@outlook.com>
2022-08-13 15:09:11 +0100

    Add functions to read new data

--- Course-01/edhec_risk_kit.py
+++ Course-01/edhec_risk_kit.py
@@ -60,6 +60,26 @@ def get_ind_returns():
     ind.columns = ind.columns.str.strip()
     return ind
 
+def get_ind_size():
+    '''
+    '''
+    ind = pd.read_csv('data/ind30_m_size.csv',
+                        header = 0,
+                        index_col = 0)
+    ind.index = pd.to_datetime(ind.index, format = '%Y%m').to_period('M')
+    ind.columns = ind.columns.str.strip()
+    return ind
+
+def get_ind_nfirms():
+    '''
+    '''
+    ind = pd.read_csv('data/ind30_m_nfirms.csv',
+                        header = 0,
+                        index_col = 0)
+    ind.index = pd.to_datetime(ind.index, format = '%Y%m').to_period('M')
+    ind.columns = ind.columns.str.strip()
+    return ind
+
 def semideviation(r):
     '''
     Returns the semideviation (negative semideviation) of r
