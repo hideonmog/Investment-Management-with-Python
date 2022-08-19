6934d46fbed75b9b75ecfcc5aa0d1f5ebd4e27aa
Mohshin Ahmed <mohshina1@outlook.com>
2022-08-20 00:05:45 +0100

    Add functions to compute CIR

--- Course-01/edhec_risk_kit.py
+++ Course-01/edhec_risk_kit.py
@@ -553,3 +553,53 @@ def funding_ratio(assets, liabilities, r):
     Computes the funding ratio of some assets given liabilities and interest rate
     '''
     return assets/pv(liabilities, r)
+
+def inst_to_ann(r):
+    ''' 
+    Converts short rate to an annualised rate 
+    '''
+    return np.expm1(r)
+
+def ann_to_inst(r):
+    ''' 
+    Convert annual rate to a short rate 
+    '''
+    return np.log1p(r)
+
+def cir(n_years = 10, n_scenarios = 1, a = 0.05, b = 0.03, sigma = 0.05, steps_per_year = 12, r_0 = None):
+    ''' 
+    Generate random interest rate eveolution over time using the CIR model
+    b and r_0 are assured to be the annualised rates, not the short rate
+    and the returned values are the annualised rates as well
+    '''
+    if r_0 is None: r_0 = b
+    r_0 = ann_to_inst(r_0)
+    dt = 1/steps_per_year
+
+    num_steps = int(n_years * steps_per_year) + 1
+    shock = np.random.normal(0, scale = np.sqrt(dt), size = (num_steps, n_scenarios))
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+
+    # for price generation
+    h = np.sqrt(a**2 + 2*sigma**2)
+    prices = np.empty_like(shock)
+
+    def price(ttm, r):
+        _A = ((2*h*np.exp((h+a)*ttm/2))/(2*h+(h+a)*(np.exp(h*ttm)-1)))**(2*a*b/sigma**2)
+        _B = (2*(np.exp(h*ttm)-1))/(2*h + (h+a)*(np.exp(h*ttm)-1))
+        _P = _A*np.exp(-_B*r)
+        return _P
+    prices[0] = price(n_years, r_0)
+    
+    for step in range(1, num_steps):
+        r_t = rates[step-1]
+        d_r_t = a*(b-r_t)*dt + sigma*np.sqrt(r_t)*shock[step]
+        rates[step] = abs(r_t + d_r_t)
+        # generate prices at time t as well ...
+        prices[step] = price(n_years-step*dt, rates[step])
+
+    rates = pd.DataFrame(data = inst_to_ann(rates), index=range(num_steps))
+    # for prices
+    prices = pd.DataFrame(data = prices, index = range(num_steps))
+    return rates, prices
