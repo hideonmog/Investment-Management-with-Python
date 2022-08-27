b04732c1a5ca1547a3d6985d3072ec52569962de
Mohshin Ahmed <mohshina1@outlook.com>
2022-08-27 16:06:41 +0100

    Fix new vectorised functions

--- Course-01/edhec_risk_kit.py
+++ Course-01/edhec_risk_kit.py
@@ -534,23 +534,23 @@ def show_cppi(n_scenarios = 50, mu = 0.07, sigma = 0.15, m = 3, floor = 0, riskf
 def discount(t, r):
     '''
     Compute the price of a pure discount bond that pays a dollar at time period t
-    and r si the per-period interest rate
+    and r is the per-period interest rate
     returns a |t| x |r| Series or DataFrame
     r can be a float, Series or DataFrame
     returns a DataFrame indexed by t
     '''
-    discounts = pd.DataFrame([(1 + r)**-i for i in t])
-    discount.index = t
+    discounts = pd.DataFrame([(r+1)**-i for i in t])
+    discounts.index = t
     return discounts
 
-def pv(flows, r):
+def pv(l, r):
     '''
     Compute the present value of a sequence of cash flows given by the time (as an index) and amounts
-    r can be a scalar, Series or DataFrame with the number of rows matching the number of rows in flows
+    r can be a scalar, or a Series or DataFrame with the number of rows matching the num of rows in flows
     '''
-    dates = flows.index
-    discounts = discounts(dates, r)
-    return discounts.multiply(flows, axis = 'rows').sum()
+    dates = l.index
+    discounts = discount(dates, r)
+    return discounts.multiply(l, axis = 'rows').sum()
 
 def funding_ratio(assets, liabilities, r):
     '''
